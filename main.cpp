4b8e6d58dde4854b742efbfb1cb1067cd676adcc
Taylor Rogers <trogers121@gmail.com>
2020-02-09 15:15:12 -0800

    Updated solution, uses unordered map for O(1) complexity

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <stdexcept>
 
 using namespace std;
 
@@ -10,12 +12,15 @@ vector<int> twoSum(vector<int>& nums, int target);
 
 int main(){
 
-    vector<int> testArray = {2,7,11,15, 16, 3, -6, 32, 1, 17};
-    int target = -3;
+    vector<int> testArray = {2,7,11,15, 16, 1, -6, 32};
+    int target = 18;
     vector<int> solution;
-
+    try{
     solution = twoSum(testArray, target);
-
+    }
+    catch(exception e){
+        cout << "Exception caught" << endl;
+    }
     cout << "Solution --> [ " << solution[0] << ", " << solution[1] << "]" << endl;
 
     return 0;
@@ -25,42 +30,37 @@ int main(){
  */
 
 vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> indices = {-1, -1};
-    
+    vector<int> indices;
+    vector<int>::iterator it;
+    unordered_map<int, int> lookUpTable;
+    //Check if vector is empty
     if(!nums.empty()){
-        
-        //Numbers for finding solution
-        int numOne;
-        int numTwo = NULL;
-
-        //Indices
-        int i;
-        int j;
-        //Starting with the 0th value in vector, compare the addition of each other value to the target value
-        for(i = 0; i < (nums.size() - 1); i++){
+        for(int i = 0; i < nums.size(); i++){
+            int addend = target - nums[i];
             
-            numOne = nums[i];
-            for(j = i + 1; j<nums.size(); j++){
-                //Adds two vals together, and compares to target.
-                int sum = numOne + nums[j];
-                cout << "Sum of " << numOne << " + " << nums[j] << " = " << sum << endl;
-                if( (sum) == target){
-                    numTwo = nums[j];
-                    break;
-                }
-                
-            }
-            
-            if(numTwo != NULL){     //Only one solution is needed --> Will need to update the output type if more solutions are wanted
-                indices[0] = i;
-                indices[1] = j;
+            unordered_map<int,int>::const_iterator indexOfAddend = lookUpTable.find(addend);
+            // cout << "Index = " << indexOfAddend;
+            if(!(indexOfAddend == lookUpTable.end())){
+                it = indices.begin();
+                it = indices.insert(it, i);
+                it = indices.insert(it, indexOfAddend->second); 
+                cout << "Solution found" << endl;
                 break;
+            } 
+            else{
+                // cout << "Not found" << endl;
+                // cout << nums[i] << " + " << addend << " = " << target << endl;
             }
+            lookUpTable.insert({ nums[i], i });
         }
     }
     else{
-        
+        throw std::invalid_argument("Input vector is empty");
     }
+    if(indices.empty()){
+        throw std::invalid_argument("Solution not found");
+    }
+
     return indices;
 }
 
