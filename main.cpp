80cf52d58049f37fe049ca214c0df5a78a5eb6f9
Taylor Rogers <trogers121@gmail.com>
2020-02-09 13:37:21 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> indices = {-1, -1};
+    
+    if(!nums.empty()){
+        
+        for(uint16_t i = 0; i < nums.size(); i ++){
+            i
+        }
+    }
+    else{
+    
+    }
+    return indices;
+}
+
+
+
+int main(){
+
+    vector<int> testArray = {2, 7, 11, 15};
+
+    twoSum(testArray, 9);
+
+    cout << "Hello" << endl;
+
+    return 0;
+}
+
+
